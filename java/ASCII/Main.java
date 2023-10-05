ecb89be2af710a775251e1e03e60f0598e53cb91
Tim Tran <toantim.developer@gmail.com>
2023-10-05 23:22:10 +0300

    add note abotu ASCII

---
+++ java/ASCII/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        String a = "abc";
+        String b = "abcd";
+        int c = a.charAt(0) - b.charAt(2);
+        System.out.println(c);
+    }
+}
